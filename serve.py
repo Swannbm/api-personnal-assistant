571cf7b725ce5cd1bd207689a64f3dc37351b5d4
Swann <swann.bouviermuller@gmail.com>
2023-03-12 16:16:49 +0100

    fix serve import

--- serve.py
+++ serve.py
@@ -1,7 +1,7 @@
 import logging
 import uvicorn
 
-from .config import PORT
+from src.config import PORT
 
 logger = logging.getLogger(__name__)
 
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def serve():
     """Run function for Scalingo, see `Procfile`"""
     logger.info(f"Running uvicorn on {PORT}")
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT, workers=1)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=PORT, workers=1)
 
 
 if __name__ == '__main__':
