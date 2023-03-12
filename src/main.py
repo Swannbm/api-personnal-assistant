4f9d2d0fb0708c069fda8039a7e6726dac01d02e
Swann <swann.bouviermuller@gmail.com>
2023-03-12 15:31:00 +0100

    add webhook route and subscribe

--- src/main.py
+++ src/main.py
@@ -11,6 +11,15 @@ async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/webhook")
+async def webhook(**kwargs):
+    mode = kwargs.get("hub.mode")
+    if mode == "subscribe":
+        challenge = kwargs.get("hub.challenge")
+        return challenge
+    return kwargs
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
